4e4c788332b27cd1e0249f3f7ec6e7bb8f861520
Arya Borhade <aryaborhade20@gmail.com>
2025-08-19 15:11:52 +0530

    Update iconMap.js

--- src/components/iconMap.js
+++ src/components/iconMap.js
@@ -1,8 +1,27 @@
-import { Code, Gavel, Megaphone, BarChart } from 'lucide-react';
+import {
+  Code,
+  Server,
+  Palette,
+  BarChart,
+  Megaphone,
+  Package,
+  Settings,
+  Edit,
+  TrendingUp,
+  Smartphone,
+  Briefcase
+} from 'lucide-react';
 
 export const iconMap = {
-  Code: Code,
-  Gavel: Gavel,
-  Megaphone: Megaphone,
-  BarChart: BarChart,
+  code: Code,
+  server: Server,
+  palette: Palette,
+  'bar-chart': BarChart,
+  megaphone: Megaphone,
+  package: Package,
+  settings: Settings,
+  edit: Edit,
+  'trending-up': TrendingUp,
+  smartphone: Smartphone,
+  briefcase: Briefcase
 };
